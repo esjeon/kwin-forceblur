911c0464dd563d64daee754f96dfaa6315de6648
Eon S. Jeon <esjeon@hyunmu.am>
2019-04-28 14:08:39 +0900

    error-free install script
     * update package only when installation fails
     * create services directory

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-plasmapkg2 -i forceblur.kwinscript
-plasmapkg2 -u forceblur.kwinscript
+plasmapkg2 -i forceblur.kwinscript || plasmapkg2 -u forceblur.kwinscript
+mkdir -pv ~/.local/share/kservices5/
 cp -vf metadata.desktop ~/.local/share/kservices5/forceblur.desktop
 
